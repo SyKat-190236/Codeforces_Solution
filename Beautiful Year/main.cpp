8f2c43cae90047712e74662f9266c4cd65741c2b
Sykat Biswas <53664820+SyKat420@users.noreply.github.com>
2021-01-03 23:45:47 +0600

    Codeforces 271A

---
+++ Beautiful Year/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    while(1)
+    {
+        n++;
+        int a=n/1000;
+        int b=n/100%10;
+        int c=n%100/10;
+        int d=n%10;
+        if(a!=b && a!=c && a!=d && b!=c && b!=d && c!=d)
+        {
+            break;
+        }
+    }
+    cout<<n;
+    return 0;
+}
